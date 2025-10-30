9a7c6c55ade11e3679b3d1d07f085caf4c55edd1
Ryo_Sawada77 <sawada@ictechsys.com>
2025-10-30 13:38:51 +0900

    型にUNSIGNEDをつけた

--- app/sql/employees.sql
+++ app/sql/employees.sql
@@ -5,7 +5,7 @@ CREATE TABLE employees(
   family_name VARCHAR(64) NOT NULL,
   address VARCHAR(255) NOT NULL,
   phone_number VARCHAR(20) NOT NULL,
-  employee_type_id INT NOT NULL,
+  employee_type_id INT UNSIGNED NOT NULL,
   PRIMARY KEY(employee_number),
   FOREIGN KEY(employee_type_id) REFERENCES employee_types(id)
 );
