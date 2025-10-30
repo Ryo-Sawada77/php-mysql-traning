9dbca5db6ce2116c21fc956581e8c88babb51d0d
Ryo_Sawada77 <sawada@ictechsys.com>
2025-10-30 17:20:23 +0900

    htmlのタグにクラスセレクタ追加

--- app/public/employees/detail.php
+++ app/public/employees/detail.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php';
+
 use Em\Service\EmployeeService;
 
 $employee_number = $_GET['employee_number'] ?? null;
@@ -12,27 +13,48 @@ if (!$employee) exit('該当する社員が見つかりません');
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <title>社員詳細</title>
+  <link rel="stylesheet" href="static/detail.css">
 </head>
+
 <body>
   <h1>社員詳細</h1>
+  <a href="index.php" class="link">一覧に戻る</a>
+  <div class="details">
+    <p>
+      <strong>社員番号:</strong> <?= htmlspecialchars($employee->employee_number) ?>
+    </p>
 
-  <p><strong>社員番号:</strong> <?= htmlspecialchars($employee->employee_number) ?></p>
-  <p><strong>氏名:</strong> <?= htmlspecialchars($employee->family_name, ENT_QUOTES, 'UTF-8') ?></p>
-  <p><strong>住所:</strong> <?= htmlspecialchars($employee->address, ENT_QUOTES, 'UTF-8') ?></p>
-  <p><strong>電話番号:</strong> <?= htmlspecialchars($employee->phone_number, ENT_QUOTES, 'UTF-8') ?></p>
-  <p><strong>社員種別名:</strong> <?= htmlspecialchars($employee->employee_type_name, ENT_QUOTES, 'UTF-8') ?></p>
-  
-  <label>Emails:</label><br>
-  <?php foreach ($employee->company_emails as $email): ?>
-      <?= htmlspecialchars($email, ENT_QUOTES, 'UTF-8') ?><br>
-  <?php endforeach; ?>
+    <p>
+      <strong>氏名:</strong> <?= htmlspecialchars($employee->family_name, ENT_QUOTES, 'UTF-8') ?>
+    </p>
 
-  <a href="edit.php?employee_number=<?= $employee->employee_number ?>">編集する</a>
-  <br>
+    <p>
+      <strong>住所:</strong> <?= htmlspecialchars($employee->address, ENT_QUOTES, 'UTF-8') ?>
+    </p>
 
-  <a href="index.php">一覧に戻る</a>
+    <p>
+      <strong>電話番号:</strong> <?= htmlspecialchars($employee->phone_number, ENT_QUOTES, 'UTF-8') ?>
+    </p>
+
+    <p>
+      <strong>社員種別名:</strong> <?= htmlspecialchars($employee->employee_type_name, ENT_QUOTES, 'UTF-8') ?>
+    </p>
+
+    <p>
+      <strong>Emails:</strong><?php foreach ($employee->company_emails as $email): ?>
+        <?= htmlspecialchars($email, ENT_QUOTES, 'UTF-8')?><br>
+      <?php endforeach; ?>
+    </p>
+    
+
+    <a class="back-link" href="edit.php?employee_number=<?= $employee->employee_number ?> ">編集する</a>
+    <br>
+    
+  </div>
 </body>
+
 </html>
--- app/public/employees/index.php
+++ app/public/employees/index.php
@@ -12,20 +12,21 @@ $employees = $service->fetchAll();
 <!DOCTYPE html>
 <html lang="ja">
 <head><meta charset="UTF-8"><title>社員一覧</title></head>
+<link rel="stylesheet" href="static/index.css">
 <body>
 <h1>社員一覧</h1>
 <!-- 社員新規登録ページに遷移 -->
 <a href="add.php">＋ 新規登録</a>
 
 <!-- 社員番号でどの社員の詳細データを出す決める -->
-<ul>
-  <?php foreach($employees as $e):?>
-  <li>
-    <?= ($e->employee_number)?>
-    <?= htmlspecialchars($e->family_name, ENT_QUOTES, 'UTF-8') ?>
-    <a href="detail.php?employee_number=<?= $e->employee_number ?>">詳細</a>
-  </li>
-  <?php endforeach?>
+<ul class="employee-list">
+  <?php foreach($employees as $e): ?>
+    <li>
+      <div class="employee-info">
+        <?= htmlspecialchars($e->employee_number . '. ' . $e->family_name) ?>
+      </div>
+      <a href="detail.php?employee_number=<?= $e->employee_number ?>" class="detail-button">詳細</a>
+    </li> <?php endforeach; ?>
 </ul>
 
 </body>
