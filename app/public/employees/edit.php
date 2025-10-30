1d9751cc9a9ec3c277ea7e640d6bbc326f342319
Ryo_Sawada77 <sawada@ictechsys.com>
2025-10-30 17:21:19 +0900

    編集処理後に詳細ページにリダイレクトするようにした

--- app/public/employees/edit.php
+++ app/public/employees/edit.php
@@ -7,23 +7,25 @@ $service = new EmployeeService();
 
 // POST送信されている場合 → 更新処理を実行
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $employee = new \Em\Employee();
-    $employee->employee_number  = $_POST['employee_number'];
-    $employee->family_name      = $_POST['family_name'];
-    $employee->address          = $_POST['address'];
-    $employee->phone_number     = $_POST['phone_number'];
-    $employee->employee_type_id = $_POST['employee_type_id'];
+  $employee = new \Em\Employee();
+  $employee->employee_number  = $_POST['employee_number'];
+  $employee->family_name      = $_POST['family_name'];
+  $employee->address          = $_POST['address'];
+  $employee->phone_number     = $_POST['phone_number'];
+  $employee->employee_type_id = $_POST['employee_type_id'];
 
-    $service->update($employee);
-    // $service->updateEmails($employee);
-    echo "更新が完了しました。<a href='index.php'>一覧へ戻る</a>";
-    exit;
+  $service->update($employee);
+  // $service->updateEmails($employee);
+
+  // 更新後に詳細ページにリダイレクト
+  header("Location: detail.php?employee_number=" . $employee->employee_number);
+  exit;
 }
 
 // GETパラメータから社員番号を取得して、その社員情報を取得
 $id = $_GET['employee_number'] ?? null;
 if (!$id) {
-    exit('社員番号が指定されていません');
+  exit('社員番号が指定されていません');
 }
 
 $employee = $service->getEmployeeDetail($id);
@@ -31,15 +33,18 @@ $employee = $service->getEmployeeDetail($id);
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <title>社員情報編集</title>
+  <link rel="stylesheet" href="static/edit.css">
 </head>
-<body>
-  <!-- 社員詳細ページに遷移 -->
-  <a href="detail.php?employee_number=<?= $employee->employee_number?>">社員詳細ページに戻る</a>
 
+<body>
   <h1>社員情報編集</h1>
+  <!-- 社員詳細ページに遷移 -->
+  <a href="detail.php?employee_number=<?= $employee->employee_number ?>" class="back-link">社員詳細ページに戻る</a>
+
   <form method="post">
     <input type="hidden" name="employee_number" value="<?= htmlspecialchars($employee->employee_number) ?>">
 
@@ -68,10 +73,10 @@ $employee = $service->getEmployeeDetail($id);
       </select>
     </div>
 
-    <div>
+    <div class="emails">
       <label>Email:</label><br>
       <?php foreach ($employee->company_emails as $i => $email): ?>
-          <input type="email" name="email[]" value="<?= htmlspecialchars($email, ENT_QUOTES, 'UTF-8') ?>"><br>
+        <input type="email" name="email[]" value="<?= htmlspecialchars($email, ENT_QUOTES, 'UTF-8') ?>"><br>
       <?php endforeach; ?>
       <!-- 新規追加用の空行 -->
       <input type="email" name="email[]" value=""><br>
@@ -79,5 +84,7 @@ $employee = $service->getEmployeeDetail($id);
 
     <button type="submit">更新</button>
   </form>
+
 </body>
+
 </html>
