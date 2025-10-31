ba920feb489cd754c54125df7740c502e0122046
Ryo_Sawada77 <sawada@ictechsys.com>
2025-10-31 17:32:51 +0900

    indexの表示をテーブルにした。

--- app/public/employees/index.php
+++ app/public/employees/index.php
@@ -1,33 +1,45 @@
 <?php
 
-use Em\Employee;
 use Em\Service\EmployeeService;
+use Em\DBConnectionFactory;
 
-require __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
-$service = new EmployeeService();
+$service = new EmployeeService(DBConnectionFactory::newConnection());
 $employees = $service->fetchAll();
- 
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head><meta charset="UTF-8"><title>社員一覧</title></head>
 <link rel="stylesheet" href="static/index.css">
 <body>
-<h1>社員一覧</h1>
-<!-- 社員新規登録ページに遷移 -->
-<a href="add.php">＋ 新規登録</a>
+  <div style="width: 1360px; margin: auto;">
+    
+    <!-- 社員新規登録ページに遷移 -->
+    <a href="add.php" style="float: right;">＋ 新規登録</a>
 
-<!-- 社員番号でどの社員の詳細データを出す決める -->
-<ul class="employee-list">
-  <?php foreach($employees as $e): ?>
-    <li>
-      <div class="employee-info">
-        <?= htmlspecialchars($e->employee_number . '. ' . $e->family_name) ?>
-      </div>
-      <a href="detail.php?employee_number=<?= $e->employee_number ?>" class="detail-button">詳細</a>
-    </li> <?php endforeach; ?>
-</ul>
+    <h1 style="text-align: left;">社員一覧</h1>
+    <!-- 社員番号でどの社員の詳細データを出す決める -->
+    <table style="width: 100%; border-collapse: collapse; border: 1px solid #ccc;">
+  <tr>
+    <th style="border: 1px solid #ccc; padding: 8px;">社員番号</th>
+    <th style="border: 1px solid #ccc; padding: 8px;">氏名</th>
+    <th style="border: 1px solid #ccc; padding: 8px;">社員種別</th>
+    <th style="border: 1px solid #ccc; padding: 8px;"></th>
+  </tr>
+  <?php foreach ($employees as $e): ?>
+  <tr>
+    <td style="border: 1px solid #ccc; padding: 8px;"><?= htmlspecialchars($e->employee_number) ?></td>
+    <td style="border: 1px solid #ccc; padding: 8px;"><?= htmlspecialchars($e->family_name) ?></td>
+    <td style="border: 1px solid #ccc; padding: 8px;"><?= htmlspecialchars($e->employee_type_name) ?></td>
+    <td style="border: 1px solid #ccc; padding: 8px;"><a class="detail-buttun" href="detail.php?employee_number=<?= ($e->employee_number) ?>">詳細</a></td>
+  </tr>
+  <?php endforeach; ?>
+</table>
+  </div>
+
+
 
 </body>
 </html>
