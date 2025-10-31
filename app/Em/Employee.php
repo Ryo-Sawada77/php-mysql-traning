5462b3df7d22b4b67598c507a0573b059123829e
Ryo_Sawada77 <sawada@ictechsys.com>
2025-10-31 17:35:00 +0900

    使わないメソッドの削除

--- app/Em/Employee.php
+++ app/Em/Employee.php
@@ -10,12 +10,4 @@ class Employee {
     public $employee_type_id;
     public $employee_type_name;     
     public $company_emails = []; 
-
-    public function __set($name, $value) {
-        if ($name === 'company_emails') { 
-            $this->company_emails = $value ? explode(',', $value) : [];
-        } else {
-            $this->$name = $value;
-        }
-    }
 }
